03df9d112aac4cc9956846197682ff7c87fed225
Wiktor Muraszko <wiktoriusrex@gmail.com>
2023-06-01 22:26:21 +0200

    added conversion from schedule to np array and back

--- DataClass/Shedule.py
+++ DataClass/Shedule.py
@@ -1,13 +1,20 @@
 import random
 
+import numpy as np
+
 from DataClass.mock_data import get_groups, get_rooms, get_courses, get_teachers
 
 all_days = ["mon", "tues", "wed", "thurs", "fri"]
-all_hours = ["8:15-9:45", "10:15-11:45", "12:15-13:45", "14:15-15:45", "16:15-17:45", "18:15-19:45", ]
+all_hours = ["8:15-9:45", "10:15-11:45", "12:15-13:45", "14:15-15:45", "16:15-17:45", "18:15-19:45"]
 
 
 class Schedule:
+    schedule = []
+
     def __init__(self):
+        self.set_blanc_schedule()
+
+    def set_blanc_schedule(self):
         self.schedule = {
             "mon": {
                 "8:15-9:45": [],
@@ -54,9 +61,33 @@ class Schedule:
     def add_position(self, GCtTR: list, day: str, hours: str):
         self.schedule[day][hours].append(GCtTR)
 
-    # def __str__(self):
-    #     for day_key in self.schedule:
-    #         for hour_key in self.schedule[day_key]:
+    def to_numpy_array(self):
+        max_classes = 0
+        for day_key in self.schedule:
+            for hour_key in self.schedule[day_key]:
+                curr = len(self.schedule[day_key][hour_key])
+                if curr > max_classes:
+                    max_classes = curr
+        np_schedule = np.empty((5, 6, max_classes, 5), dtype=float)
+        np_schedule.fill(np.nan)
+        for idx_day, day_key in enumerate(self.schedule):
+            for idx_hour, hour_key in enumerate(self.schedule[day_key]):
+                for idx_GCtTR, GCtTR in enumerate(self.schedule[day_key][hour_key]):
+                    for idx, element in enumerate(GCtTR):
+                        np_schedule[idx_day, idx_hour, idx_GCtTR, idx] = element["id"]
+        return np_schedule
+
+    @staticmethod
+    def to_schedule(array: np.array):
+        new_schedule = Schedule()
+        for day_idx, day in enumerate(array):
+            for hour_idx, hour in enumerate(day):
+                for GCtTR_idx, GCtTR in enumerate(hour):
+                    if not np.any(np.isnan(GCtTR), axis=0):
+                        temp_GCtTR = [get_groups()[int(GCtTR[0])], get_courses()[int(GCtTR[1])], {"id": int(GCtTR[2])},
+                                      get_teachers()[int(GCtTR[3])], get_rooms()[int(GCtTR[4])]]
+                        new_schedule.add_position(temp_GCtTR, all_days[day_idx], all_hours[hour_idx])
+        return new_schedule
 
 
 class ScheduleGenerator:
@@ -68,6 +99,7 @@ class ScheduleGenerator:
         self.rooms = rooms if rooms else get_rooms()
 
     def generate(self):
+        self.schedule.set_blanc_schedule()
         for group in self.groups:
             group_courses = group["courses"]
             for course in group_courses:
@@ -84,7 +116,7 @@ class ScheduleGenerator:
                         else:
                             possible_rooms.append(room)
 
-                    schedule_item = [group, course, _type, random.choice(possible_teachers),
+                    schedule_item = [group, self.courses[course], {"id": _type}, random.choice(possible_teachers),
                                      random.choice(possible_rooms)]
                     self.schedule.add_position(schedule_item, random.choice(all_days), random.choice(all_hours))
-
+        return self
--- main.py
+++ main.py
@@ -6,43 +6,19 @@ function_inputs = [4, -2, 3.5, 5, -11, -4.7]
 desired_output = 44
 
 
-def fitness_func(my_ga_instance, solution, solution_idx):
-    output = numpy.sum(solution * function_inputs)
-    fitness = 1.0 / numpy.abs(output - desired_output)
-    return fitness
+def fitness_func(my_ga_instance, solution: Schedule, solution_idx):
+    for day_key in solution.schedule:
+        for hour_key in solution.schedule[day_key]:
+            for element in solution.schedule[day_key][hour_key]:
+                group = element[0]
 
 
-num_generations = 50
-num_parents_mating = 4
+    return 0
 
-fitness_function = fitness_func
 
-sol_per_pop = 8
-num_genes = len(function_inputs)
+_mySchedules = ScheduleGenerator()
+np_test = _mySchedules.generate().schedule.to_numpy_array()
+my_schedule = Schedule.to_schedule(np_test)
+print("koniec")
 
-init_range_low = -2
-init_range_high = 5
 
-parent_selection_type = "sss"
-keep_parents = 1
-
-crossover_type = "single_point"
-
-mutation_type = "random"
-mutation_percent_genes = 10
-
-ga_instance = pygad.GA(num_generations=num_generations,
-                       num_parents_mating=num_parents_mating,
-                       fitness_func=fitness_function,
-                       sol_per_pop=sol_per_pop,
-                       num_genes=num_genes,
-                       init_range_low=init_range_low,
-                       init_range_high=init_range_high,
-                       parent_selection_type=parent_selection_type,
-                       keep_parents=keep_parents,
-                       crossover_type=crossover_type,
-                       mutation_type=mutation_type,
-                       mutation_percent_genes=mutation_percent_genes)
-
-ga_instance.run()
-ga_instance.plot_fitness()
