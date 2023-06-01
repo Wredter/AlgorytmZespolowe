993ce0ad9dfc2db846b35aad745ca96665dcb103
Wiktor Muraszko <wiktoriusrex@gmail.com>
2023-06-01 20:40:39 +0200

    generating random schedules

--- DataClass/Shedule.py
+++ DataClass/Shedule.py
@@ -1,3 +1,11 @@
+import random
+
+from DataClass.mock_data import get_groups, get_rooms, get_courses, get_teachers
+
+all_days = ["mon", "tues", "wed", "thurs", "fri"]
+all_hours = ["8:15-9:45", "10:15-11:45", "12:15-13:45", "14:15-15:45", "16:15-17:45", "18:15-19:45", ]
+
+
 class Schedule:
     def __init__(self):
         self.schedule = {
@@ -43,5 +51,40 @@ class Schedule:
             }
         }
 
-    def add_position(self, GCT: list, day: str, hours: str):
-        self.schedule[day][hours].append(GCT)
+    def add_position(self, GCtTR: list, day: str, hours: str):
+        self.schedule[day][hours].append(GCtTR)
+
+    # def __str__(self):
+    #     for day_key in self.schedule:
+    #         for hour_key in self.schedule[day_key]:
+
+
+class ScheduleGenerator:
+    def __init__(self, teachers=None, courses=None, groups=None, rooms=None):
+        self.schedule = Schedule()
+        self.teachers = teachers if teachers else get_teachers()
+        self.groups = groups if groups else get_groups()
+        self.courses = courses if courses else get_courses()
+        self.rooms = rooms if rooms else get_rooms()
+
+    def generate(self):
+        for group in self.groups:
+            group_courses = group["courses"]
+            for course in group_courses:
+                possible_teachers = []
+                for teacher in self.teachers:
+                    if course in teacher["courses"]:
+                        possible_teachers.append(teacher)
+                for _type in self.courses[course]["types"]:
+                    possible_rooms = []
+                    for room in self.rooms:
+                        if _type == 1:
+                            if room["room_type"] == 1:
+                                possible_rooms.append(room)
+                        else:
+                            possible_rooms.append(room)
+
+                    schedule_item = [group, course, _type, random.choice(possible_teachers),
+                                     random.choice(possible_rooms)]
+                    self.schedule.add_position(schedule_item, random.choice(all_days), random.choice(all_hours))
+
---
+++ DataClass/mock_data.py
@@ -0,0 +1,100 @@
+teachers = [
+    {"id": 0,
+     "courses": [0, 1, 2, 3],
+     "name": "Dariusz Mróz"},
+    {"id": 1,
+     "courses": [8, 7, 6, 5],
+     "name": "Marek Ksiądź"},
+    {"id": 2,
+     "courses": [3, 4, 5, 6],
+     "name": "Kamil Drama"},
+]
+courses = [
+    {"id": 0,
+     "types": [0, 1, 2],
+     "name": "Matematyka"},
+    {"id": 1,
+     "types": [0, 1],
+     "name": "Fizyka"},
+    {"id": 2,
+     "types": [0],
+     "name": "Teoria mechaniki kwantowej"},
+    {"id": 3,
+     "types": [0, 1],
+     "name": "Elektoronika"},
+    {"id": 4,
+     "types": [0, 1],
+     "name": "Podstawy programowania"},
+    {"id": 5,
+     "types": [0, 1],
+     "name": "Projektowanie Zespołowe"},
+    {"id": 6,
+     "types": [0, 1],
+     "name": "Silniki baz danych"},
+    {"id": 7,
+     "types": [0, 1],
+     "name": "Implementacje przemysłowe"},
+    {"id": 8,
+     "types": [0, 1],
+     "name": "Systemy Wbudowane"}
+]
+
+# types:
+# 0 = wykład w [0,1]
+# 1 = labolatorium w [1]
+# 2 = ćwiczenia w [0,1]
+groups = [
+    {"id": 0,
+     "courses": [0, 1, 2, 3, 4, 5, 6],
+     "name": "I"},
+    {"id": 1,
+     "courses": [0, 1, 2, 3, 4, 5, 6],
+     "name": "II"},
+    {"id": 2,
+     "courses": [0, 1, 7, 3, 4, 5, 6],
+     "name": "III"},
+    {"id": 3,
+     "courses": [0, 1, 2, 3, 4, 7, 6],
+     "name": "IV"},
+    {"id": 4,
+     "courses": [0, 1, 2, 3, 4, 5, 6],
+     "name": "V"},
+    {"id": 5,
+     "courses": [0, 1, 2, 8, 4, 5, 6],
+     "name": "VI"},
+    {"id": 6,
+     "courses": [0, 1, 2, 3, 4, 7, 8],
+     "name": "VI"}
+]
+rooms = [
+    {"id": 0,
+     "room_type": 0,
+     "name": "101"},
+    {"id": 1,
+     "room_type": 0,
+     "name": "102"},
+    {"id": 2,
+     "room_type": 1,
+     "name": "205"},
+    {"id": 3,
+     "room_type": 1,
+     "name": "206"},
+]
+# room_types:
+# 0 = sala wykładowa
+# 1 = labolatorium_komputerowe
+
+def get_teachers():
+    return teachers
+
+
+def get_courses():
+    return courses
+
+
+def get_groups():
+    return groups
+
+
+def get_rooms():
+    return rooms
